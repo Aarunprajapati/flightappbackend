9a270ab5627aba2b9333b1c4162d04032c1628f3
Arun <arunprajapati8502@gmail.com>
2024-04-04 15:42:02 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -10,10 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const DATABASEURL = process.env.DATABASE_URL;
 connectDB(DATABASEURL);
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+
 app.use(
   cors({
     origin: "http://flightapp-wine.vercel.app",
