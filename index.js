17eac8b7160eb96adf8597d3bc65283ef84b93ab
Arun <arunprajapati8502@gmail.com>
2024-04-17 14:38:36 +0530

    update cros

--- index.js
+++ index.js
@@ -15,15 +15,14 @@ app.use(
   cors({
     origin: 'https://flightapp-wine.vercel.app',
     credentials: true,
+    sameSite: 'None',
     preflightContinue: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   }),
 );
 app.use(express.urlencoded({ limit: "16kb" }));
 app.use(express.json({ limit: "16kb" }));
-
 app.use("/api/user", userRoutes);
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
