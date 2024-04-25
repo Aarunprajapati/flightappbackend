cfcf68f9b429ac62cf07983bde72f7758e0b872d
Rohitmeerwal <rohitkumar302013@gmail.com>
2024-04-25 12:09:19 +0530

    chnages in the booking api

--- models/userModel.js
+++ models/userModel.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
       gender: {
         type: String,
         required: true,
-        enum: ["male", "female"],
+       
       },
       profilePic: {
         type: String,
