cafb2e80637ddbb54bdf2d396c456a6bf8ce522e
Arun <arunprajapati8502@gmail.com>
2024-05-01 17:28:29 +0530

    add domain in cookie options

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,6 +46,8 @@ const userController = {
         path: "/",
         secure: true,
         sameSite: "none",
+        maxAge: 86400 * 1000,
+        Domain:`${process.env.FRONTEND_URL}`
 
       };
       const accessToken = await user.generateAccessToken();
@@ -81,6 +83,7 @@ const userController = {
           secure: true,
           sameSite: "None",
           maxAge: 86400 * 1000,
+          Domain:`${process.env.FRONTEND_URL}`
         };
 
         const { accessToken } = await generateFreshAccessToken(user._id);
